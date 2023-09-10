7bf7194d2df41d1e0acb19839cd987cd1482e9c7
Javier Hermosilla <jvrhermosilla@gmail.com>
2023-09-09 23:33:21 -0300

    channel blue print

---
+++ tif_discord/routes/channel_bp.py
@@ -0,0 +1,10 @@
+from flask import Blueprint
+from ..controllers.channel_controls import ChannelController
+
+channel_bp = Blueprint('channel_bp', __name__)
+
+channel_bp.route('/', methods=['GET'])(ChannelController.get_all)
+channel_bp.route('/<int:id_channel>', methods=['GET'])(ChannelController.get_one)
+channel_bp.route('/', methods=['POST'])(ChannelController.create_channel)
+channel_bp.route('/<int:id_channel>', methods=['PUT'])(ChannelController.update_channel)
+
