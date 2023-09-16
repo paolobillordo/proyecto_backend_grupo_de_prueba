6d891fa9b253bb51404997364177c9f4872f66ab
Paolo Billordo <paolobillordo@gmail.com>
2023-09-16 00:17:43 -0300

    borrado de prints

--- tif_discord/controllers/server_controls.py
+++ tif_discord/controllers/server_controls.py
@@ -30,11 +30,9 @@ class ServerController:
             return jsonify(result.serialize()), 200
         
     @classmethod
-    def create_server(cls):
-        print("1")
-        data= request.json
-        data['id_user'] = session.get("id_user")
-        print(data)
+    def create_server(cls):        
+        data = request.json
+        data['id_user'] = session.get("id_user")        
         server = Servers(**data)
         Servers.create_server(server)                        
         return {"mensaje": "Servidor creado con exito"},201
